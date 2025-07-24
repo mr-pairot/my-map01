9167c4041dbbcb01d9a337e51e7224e38b2e40b7
mr-pairot <mr.pairot@gmail.com>
2025-07-24 20:04:28 +0700

    Create setGeoman.js

---
+++ js/setGeoman.js
@@ -0,0 +1,44 @@
+// setGeoman.js
+
+let isGeomanVisible = false;
+
+function setupGeomanToggle(map) {
+  // สร้าง control ตอนเริ่ม
+  map.pm.addControls({
+    position: 'topleft',
+    drawMarker: true,
+    drawPolygon: true,
+    drawPolyline: true,
+    drawCircle: true,
+    drawRectangle: true,
+    drawCircleMarker: false,
+    editMode: false,
+    dragMode: true,
+    cutPolygon: false,
+    deleteLayer: true,
+    rotateMode: false,
+    oneBlock: false
+  });
+
+  // ซ่อนไว้ก่อน (ไม่ให้แสดง control ตอนโหลด)
+  setTimeout(() => {
+    document.querySelector('.leaflet-pm-toolbar')?.classList.remove('show');
+  }, 100);
+
+  const btn = document.getElementById('geoBtn');
+
+  btn.addEventListener('click', () => {
+    const toolbar = document.querySelector('.leaflet-pm-toolbar');
+    if (!toolbar) return;
+
+    isGeomanVisible = !isGeomanVisible;
+
+    if (isGeomanVisible) {
+      toolbar.classList.add('show');
+      btn.innerText = '⬆️';
+    } else {
+      toolbar.classList.remove('show');
+      btn.innerText = '⬇️';
+    }
+  });
+}
